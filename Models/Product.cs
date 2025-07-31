028b4f3b46c0496a86c6e9146347d62a8792d530
Toasty <toastedtoast0090@gmail.com>
2025-07-30 22:13:13 -0700

    added in required to Title string so that warning doesn't pop up and basic validation included.

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,7 +13,7 @@ namespace eCommerce.Models
 
         [StringLength(50, ErrorMessage = "The product name cannot exceed 50 characters.")]
         //The name of the product
-        public string Title { get; set; }
+        public required string Title { get; set; }
 
         [Range (0, 10_000)]
         //the price of the product with an upper limit of 10,000 $

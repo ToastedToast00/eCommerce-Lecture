68fa556cd69e655525895f6f31fb90e74b8739c6
ToastedToast0090 <toastedtoast0090@gmail.com>
2025-08-28 02:04:50 -0700

    Added Product Controller and create view, made form to add custom products into database (#16)

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ProductDbContext _context;
+    public ProductController(ProductDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product p)
+    {
+        if(ModelState.IsValid)
+        {
+            _context.Products.Add(p);           //Add to database
+            await _context.SaveChangesAsync();  //Save changes
+
+            return RedirectToAction("Index");
+        }
+        return View(p);
+    }
+}

b3cedd873371350c389b265060b4e5c85d6eb84f
Toasty <toastedtoast0090@gmail.com>
2025-08-29 00:02:01 -0700

    Created edit action inside of product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -39,4 +39,19 @@ public class ProductController : Controller
         }
         return View(p);
     }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        Product? product = _context.Products
+            .Where(p => p.ProductId == id)
+            .FirstOrDefault();
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
 }

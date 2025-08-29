493e46934e36aea862a028fb757e94da4234c879
Toasty <toastedtoast0090@gmail.com>
2025-08-29 08:15:50 -0700

    Created Registration View model inside of member.cs
    Made Register.cshtml to create new users
    
    Made Member Controller and added a view for register.cshtml

--- Models/Member.cs
+++ Models/Member.cs
@@ -41,3 +41,39 @@ public class Member
     
     public DateOnly DateOfBirth { get; set; }
 }
+
+public class RegistrationViewModel
+{
+    /// <summary>
+    /// public facing username for the member.
+    /// aplhanumeric only.
+    /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric only")]
+    [StringLength(25, ErrorMessage = "Username must be less than 26 characters")]
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Email for the member
+    /// </summary>
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The members password
+    /// </summary>
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Your password must fit between 6 and 50 keys.")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    /// <summary>
+    /// the date of birth of the member
+    /// </summary>
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+}
